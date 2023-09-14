d34d8ccc814fd76b0863274a77f858aad108708b
IgorS <sok.igor4@gmail.com>
2023-09-15 00:59:15 +0200

    Revert "Merge branch 'main' of https://github.com/IgorSok4/LivinnX_Booking_Page"
    This reverts commit 6411ada9b812207bbd6de5310631eff121bc6187, reversing
    changes made to e7c79ef86e5199f2bf52cc9776a1523c40713bf5.

--- management_panel/urls.py
+++ management_panel/urls.py
@@ -4,11 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.admin_login, name='admin_login'),
     path('dashboard/', views.admin_dashboard, name='admin_dashboard'),
-<<<<<<< HEAD
     path('tenants/', views.TenantsListView.as_view(), name='admin_tenants'),
     path('admin/tenants/toggle_user_active/<int:user_id>/', views.toggle_user_active, name='toggle_user_active'),
 ]
-=======
-    path(r'tenants/^$', views.TenantsListView.as_view(), name='admin_tenants'),
-]
->>>>>>> b8b7a38f9007bd786575cef10ef0da2b796630b1
